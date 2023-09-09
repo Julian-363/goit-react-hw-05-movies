25ea7368fc26567bbd551b1f28facb7753f8331d
Julian-363 <juliangil333@gmail.com>
2023-09-08 20:19:44 -0500

    tarea-5-entraga-otravez

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
-import { API_KEY } from './services/Movies.API';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
